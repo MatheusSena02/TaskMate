fe4a44cb6d22e344e691f9dbf3bf9f8d699d7585
Matheus Sena <matheussena02@gmail.com>
2025-10-03 14:20:08 -0300

    feat(TaskMate.Infrastructure): Implement methods of IRepository in the class InMemoryRepository

--- TaskMate/TaskMate.Infrastructure/InMemoryRepository.cs
+++ TaskMate/TaskMate.Infrastructure/InMemoryRepository.cs
@@ -3,10 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TaskMate.Core;
 
 namespace TaskMate.Infrastructure
 {
-    internal class InMemoryRepository
+    public class InMemoryRepository <T> : IRepository<T>
     {
+        private List<T> _taskList;
+        public List<T> TaskList { get { return _taskList; } }   
+        public InMemoryRepository()
+        {
+            _taskList = new List<T>();
+        }
+        public void Add(T item)
+        {
+            _taskList.Add(item);
+        }
+
+        public T GetById(Guid id)
+        {
+
+            return _taskList.FirstOrDefault(item => item is BaseTask task && task.Id == id);
+        }
     }
 }
