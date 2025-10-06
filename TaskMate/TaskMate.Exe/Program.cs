4711535bc084df7e72e2c54757832c5b8782e287
Matheus Sena <matheus.sena@renova.net.br>
2025-10-06 09:21:23 -0300

    feat: add new class TaskMate.Exe

---
+++ TaskMate/TaskMate.Exe/Program.cs
@@ -0,0 +1,11 @@
+﻿using TaskMate.Core;
+
+Console.Write("Digite o nome: ");
+string title = Console.ReadLine();
+Console.Write("Digite a data: ");
+string dateInsert = Console.ReadLine();
+var startingDate = DateOnly.Parse(dateInsert);
+
+var testSimpleTask = new SimpleTask(title, startingDate);
+testSimpleTask.GetDetails();
+
