f569e78e063c631530392ed085e0aa0f31b8239d
Matheus Sena <matheus.sena@renova.net.br>
2025-10-08 09:58:22 -0300

    feat: Implement method to print and handle subtask details

--- TaskMate/TaskMate.Core/TaskService.cs
+++ TaskMate/TaskMate.Core/TaskService.cs
@@ -33,21 +33,42 @@ namespace TaskMate.Core
                         Console.WriteLine("\t- Subtarefas:");
                         foreach(var subtask in simpleTask.Subtask)
                         {
-                            Console.Write("\t  ");
-                            ListSimpleTask(simpleTask);
+                            ListSubtask(subtask);
                         }
                     }
                 }else if(task is DeadLineTask deadLineTask)
                 {
-                    //Executa ListDeadLineTask
+                    if(deadLineTask.Subtask.Count == 0)
+                    {
+                        ListDeadLineTask(deadLineTask);
+                    }else if(deadLineTask.Subtask.Count > 0)
+                    {
+                        ListDeadLineTask(deadLineTask);
+                        Console.WriteLine("\t- Subtarefas:");
+                        foreach(var subtask in deadLineTask.Subtask)
+                        {
+                            ListSubtask(subtask);
+                        }
+                    }
                 }else if(task is RecurringTask recurringTask)
                 {
-                    //List RecurringTask
+                    if(recurringTask.Subtask.Count == 0)
+                    {
+                        ListRecurringTask(recurringTask);
+                    }else if(recurringTask.Subtask.Count > 0)
+                    {
+                        ListRecurringTask(recurringTask);
+                        Console.WriteLine("\t- Subtarefas:");
+                        foreach (var subtask in recurringTask.Subtask)
+                        {
+                            ListSubtask(subtask);
+                        }
+                    }
                 }
             }
         }
 
-        public void ListSimpleTask(SimpleTask simpleTask)
+        public void ListSimpleTask(BaseTask simpleTask)
         {
             if (simpleTask.TaskStatus != statusOption.CONCLUIDA)
             {
@@ -55,26 +76,52 @@ namespace TaskMate.Core
             }
             else
             {
-                Console.WriteLine($"[ID: {simpleTask.Id} [✅] {simpleTask.Title}\n\t- Descrição: {simpleTask.Description}\n");
+                Console.WriteLine($"[ID: {simpleTask.Id} [✅] {simpleTask.Title}\n\t- Descrição: {simpleTask.Description}");
             }
         }
 
-        public void ListDeadLineTask(IRepository<BaseTask> irepository)
+        public void ListDeadLineTask(BaseTask deadLineTask)
         {
-            var taskList = irepository.GetAllTasks();
-            foreach (var task in taskList)
+            if(deadLineTask.TaskStatus != statusOption.CONCLUIDA)
             {
-                if (task is SimpleTask simpleTask)
-                {
-                    if (simpleTask.TaskStatus != statusOption.CONCLUIDA)
-                    {
-                        Console.WriteLine($"[ID: {simpleTask.Id} [⬜] {simpleTask.Title}\n\t-Descrição: {simpleTask.Description}\n");
-                    }
-                    else
-                    {
-                        Console.WriteLine($"[ID: {simpleTask.Id} [✅] {simpleTask.Title}\n\t-Descrição: {simpleTask.Description}\n");
-                    }
-                }
+                Console.WriteLine($"[ID: {deadLineTask.Id} [⬜] {deadLineTask.Title}\n\t- Descrição: {deadLineTask.Description}\n\t- (Prazo: {deadLineTask.DeadLineDate})");
+            }
+            else
+            {
+                Console.WriteLine($"[ID: {deadLineTask.Id} [✅] {deadLineTask.Title}\n\t- Descrição: {deadLineTask.Description}\n\t- (Prazo: {deadLineTask.DeadLineDate})");
+            }
+        }
+
+        public void ListRecurringTask(BaseTask recurringTask)
+        {
+            if(recurringTask.TaskStatus != statusOption.CONCLUIDA)
+            {
+                Console.WriteLine($"[ID: {recurringTask.Id} [⬜] {recurringTask.Title}\n\t- Descrição: {recurringTask.Description}\n\t- (Recorrência: {recurringTask.SelectedRecurringDays}");
+            }
+            else
+            {
+                Console.WriteLine($"[ID: {recurringTask.Id} [✅] {recurringTask.Title}\n\t- Descrição: {recurringTask.Description}\n\t- (Recorrência: {recurringTask.SelectedRecurringDays}");
+            }
+        }
+
+        public void ListSubtask(BaseTask subtask)
+        {
+            if(subtask is SimpleTask simpleTaskSubtask)
+            {
+                Console.Write("\t  - ");
+                ListSimpleTask(simpleTaskSubtask);
+                Console.WriteLine();
+
+            }else if(subtask is  DeadLineTask deadLineTaskSubtask)
+            {
+                Console.Write("\t  - ");
+                ListDeadLineTask(deadLineTaskSubtask);
+                Console.WriteLine();
+            }else if(subtask is RecurringTask recurringTaskSubtask)
+            {
+                Console.Write("\t  - ");
+                ListDeadLineTask(recurringTaskSubtask);
+                Console.WriteLine();
             }
         }
     }
