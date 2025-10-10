18a126c24def242b94663f415c0f6cd7ac1157cd
Matheus Sena <matheus.sena@renova.net.br>
2025-10-10 11:10:41 -0300

    feat: Implement virtual method CreateTask in BaseTask

--- TaskMate/TaskMate.Console/UserInterface.cs
+++ TaskMate/TaskMate.Console/UserInterface.cs
@@ -10,12 +10,25 @@ namespace TaskMate.UI
 {
     public class UserInterface
     {
+        internal string[] selectOptionsTypeOfTask = { "Tarefa Simples", "Tarefa com Prazo", "Tarefa Recorrente" };
+
         public void ListDetailsOfTask(InMemoryRepository<BaseTask> taskList)
         {
             Console.Write("Digite o ID da tarefa para ver os detalhes: ");
             var requestId = Guid.Parse(Console.ReadLine()); 
             var requestTask = taskList.GetTaskById(requestId);
             requestTask.GetDetails();
+        }
+
+        public void CreateTask()
+        {
+            Console.WriteLine("Qual tipo de tarefa você deseja criar?\r");
+            Console.WriteLine($"  [1] Tarefa Simples");
+            Console.WriteLine($"  [2] Tarefa com Prazo");
+            Console.WriteLine($"  [3] Tarefa Recorrente\n");
+            Console.Write("Digite sua opção: ");
+            int selectedOption = Convert.ToInt32(Console.ReadLine());   
+
         }
     }
 }
