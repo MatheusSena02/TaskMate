09a6f9998d922e2ee2a11ca12c2a2ba7d0e16f63
Matheus Sena <matheus.sena@renova.net.br>
2025-10-10 09:46:15 -0300

    feat: Override PrintTask method in SimpleTask

--- TaskMate/TaskMate.Core/SimpleTask.cs
+++ TaskMate/TaskMate.Core/SimpleTask.cs
@@ -11,5 +11,17 @@ namespace TaskMate.Core
         public SimpleTask(string title, DateOnly startingDate, string description = "") : base(title, startingDate, description)
         {
         }
+
+        public override void PrintTask()
+        {
+            if (TaskStatus != statusOption.CONCLUIDA)
+            {
+                Console.WriteLine($"[ID: {Id}]\n[ ] {Title}\n\t- Descrição: {Description}");
+            }
+            else
+            {
+                Console.WriteLine($"[ID: {Id}]\n[X] {Title}\n\t- Descrição: {Description}");
+            }
+        }
     }
 }
