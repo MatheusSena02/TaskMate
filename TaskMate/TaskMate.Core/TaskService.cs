9e5df46e7c146cb123098db1e530b2dfd613cdd6
Matheus Sena <matheus.sena@renova.net.br>
2025-10-13 10:01:49 -0300

    refactor: Improve method logic by reducing scope of CreateTask method and extracting specialized methods

--- TaskMate/TaskMate.Core/TaskService.cs
+++ TaskMate/TaskMate.Core/TaskService.cs
@@ -43,43 +43,13 @@ namespace TaskMate.Core
             switch(selectedOption)
             {
                 case 1:
-                    Console.WriteLine("\n>> Criando uma nova Tarefa Simples...\r\n");
-                    Console.Write("Digite o título da tarefa: ");
-                    string titleSimpleTask = Console.ReadLine();
-                    Console.Write("\nDigite a descrição (opcional): ");
-                    string descriptionSimpleTask = Console.ReadLine();
-                    Console.Write("\nDigite a data de início da tarefa: ");
-                    var startingDateSimpleTask = DateOnly.Parse(Console.ReadLine());
-                    _irepository.AddTask(new SimpleTask(titleSimpleTask, startingDateSimpleTask, descriptionSimpleTask));
+                    _irepository.AddTask(TaskCreator.CreateSimpleTask());
                     break;
                 case 2:
-                    Console.WriteLine("\n>> Criando uma nova Tarefa com Prazo...\r\n");
-                    Console.Write("Digite o título da tarefa: ");
-                    string titleDeadLineTask = Console.ReadLine();
-                    Console.Write("\nDigite a descrição (opcional): ");
-                    string descriptionDeadLineTask = Console.ReadLine();
-                    Console.Write("\nDigite a data de início da tarefa: ");
-                    var startingDateDeadLineTask = DateOnly.Parse(Console.ReadLine());
-                    Console.Write("\nDigite a data de término da tarefa: ");
-                    var endDate = DateOnly.Parse(Console.ReadLine());
-                    _irepository.AddTask(new DeadLineTask(titleDeadLineTask, startingDateDeadLineTask, endDate, descriptionDeadLineTask));
+                    _irepository.AddTask(TaskCreator.CreateDeadLineTask()); 
                     break;
                 case 3:
-                    Console.WriteLine("\n>> Criando uma nova Tarefa Recorrente...\r\n");
-                    Console.Write("Digite o título da tarefa: ");
-                    string titleRecurringTask = Console.ReadLine();
-                    Console.Write("\nDigite a descrição (opcional): ");
-                    string descriptionRecurringTask = Console.ReadLine();
-                    Console.Write("\nDigite a data de início da tarefa: ");
-                    var startingDateRecurringTask = DateOnly.Parse(Console.ReadLine());
-                    Console.Write("\n\tSelecione as opções de recorrência da tarefa : \n");
-                    Console.Write("----------------------------------------------------------------------------------");
-                    Console.Write("\n  | 1 - Todos os dias | 2 - Apenas dias úteis | 3 - Apenas ao finais de semana |\n");
-                    Console.Write("\t     | 4 - Segunda, Quarta e Sexta | 5 - Terça e Quinta |\n");
-                    Console.Write("----------------------------------------------------------------------------------");
-                    Console.Write("\n\t\t\tDigite a opção: ");
-                    int selectedRecurringOption = Convert.ToInt16(Console.ReadLine());
-                    _irepository.AddTask(new RecurringTask(titleRecurringTask, startingDateRecurringTask, selectedRecurringOption, descriptionRecurringTask));
+                    _irepository.AddTask(TaskCreator.CreateRecurringTask());
                     break;
                 default:
                     Console.WriteLine("Não foi possível criar a tarefa...");
