65f7561ff7ea0abbec674168f9d02f24d50a9181
Matheus Sena <matheussena02@gmail.com>
2025-10-14 17:12:11 -0300

    refactor(IRepository): constrain generic type T to BaseTask

--- TaskMate/TaskMate.Core/IRepository.cs
+++ TaskMate/TaskMate.Core/IRepository.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace TaskMate.Core
 {
-    public interface IRepository<T>
+    public interface IRepository<T> where T : BaseTask
     {
         T? GetTaskById(Guid id);
         List<T> GetAllTasks();
