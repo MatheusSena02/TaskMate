42fc21894cb742f3e7fa26cfab70bc2caba4f9fd
Matheus Sena <matheus.sena@renova.net.br>
2025-10-17 09:26:06 -0300

    refactor(infra): clean up and optimize InMemoryRepository logic
    Refactor methods in InMemoryRepository to strictly adhere to Clean Architecture principles.
    
    - Ensure repository methods only handle data access (CRUD operations) and do not contain business logic or user interface interaction (I/O).
    - Improve the internal logic and code quality of the implemented methods for better performance and readability.
    - Update method signatures and implementations to reflect the removal of subtask management logic from the IRepository interface.

--- TaskMate/TaskMate.Infrastructure/InMemoryRepository.cs
+++ TaskMate/TaskMate.Infrastructure/InMemoryRepository.cs
@@ -11,117 +11,42 @@ namespace TaskMate.Infrastructure
     {
         public List<T> TaskList { get; set; } = new();
          
-        public void AddTask(T item)
+        public void AddTask(T task)
         {
-            TaskList.Add(item);
+            TaskList.Add(task);
         }
 
-        public void AddSubstask(Guid idTask, T item)
+        public T GetTaskById(Guid id)
         {
-            var selectedTask = GetTaskById(idTask);
-            selectedTask.Subtask.Add(item);
-        }
-
-        public T? GetTaskById(Guid id)
-        {
-            var requestTask = _taskList.FirstOrDefault(x => x.Id == id);
-            if (requestTask != null)
+            var requestTask = TaskList.FirstOrDefault(x => x.Id == id);
+            if(requestTask == null)
             {
-                return requestTask;
+                throw new ArgumentException("Não foi possível encontrar uma tarefa correspondente ao ID informado");
             }
-            else
-            {
-                return default;
-            }
+            return requestTask;
         }
 
         public List<T> GetAllTasks()
         {
-            return _taskList;
+            return TaskList;
         }
 
         public void RemoveTask(Guid id)
         {
-            var taskToRemove = _taskList.FirstOrDefault(t => t.Id ==  id);
-            if (taskToRemove != null)
+            var taskToRemove = TaskList.FirstOrDefault(t  => t.Id == id);
+            if(taskToRemove == null)
             {
-                _taskList.Remove(taskToRemove);
+                throw new ArgumentException("Não foi possível encontrar uma tarefa correspondente ao ID informado");
+            }
+            else
+            {
+                TaskList.Remove(taskToRemove);
             }
         }
 
         public void CleanList()
         {
-            _taskList.Clear();
-        }
-
-        public void CleanSubstaskList(Guid id) 
-        {
-            var searchTask = _taskList.FirstOrDefault(a => a.Id == id);
-            searchTask.Subtask.Clear();
-        }
-
-        public void EditTitle(Guid id, string newTitle)
-        {
-            if (!String.IsNullOrEmpty(newTitle))
-            {
-                var selectedTask = GetTaskById(id);
-                if(selectedTask == null)
-                {
-                    throw new ArgumentException("Não foi impossível encontrar a tarefa. Verifique o Id e digite novamente");
-                }
-                else
-                {
-                    selectedTask.Title = newTitle;
-                }
-            }
-        }
-
-        public void EditDescription(Guid id, string newDescription)
-        {
-            if (!String.IsNullOrEmpty(newDescription))
-            {
-                var selectedTask = GetTaskById(id);
-                if(selectedTask == null)
-                {
-                    throw new ArgumentException("Não foi impossível encontrar a tarefa. Verifique o Id e digite novamente");
-                }else
-                {
-                    selectedTask.Description = newDescription;
-                }
-            }
-        }
-
-        public void EditStartingDate(Guid id, string newStartingDate)
-        {
-            if (!String.IsNullOrEmpty(newStartingDate))
-            {
-                var selectedTask = GetTaskById(id);
-                if(selectedTask == null)
-                {
-                    throw new ArgumentException("Não foi impossível encontrar a tarefa. Verifique o Id e digite novamente");
-                }
-                else
-                {
-                    try
-                    {
-                        var formattedDate = DateOnly.TryParseExact(Console.ReadLine(), "dd/MM/yyyy", null, System.Globalization.DateTimeStyles.None, out DateOnly isValidStartingDate);
-                        if(formattedDate == true)
-                        {
-                            selectedTask.StartingDate = isValidStartingDate;
-                        }
-                        else
-                        {
-                            throw new ArgumentException("Invalid date range: End date precedes start date");
-                        }
-                    }
-                    catch(Exception ex)
-                    {
-                        Console.WriteLine(ex.Message);
-                    }
-                    
-                    
-                }
-            }
+            TaskList.Clear();
         }
     }
 }
