971607dde5d4e86539ef6061ee9ceb26d4e12b1e
Matheus Sena <matheus.sena@renova.net.br>
2025-10-17 10:40:57 -0300

    feat(TaskMate.UI): introduce ITaskRender interface for task visualization
    Add the ITaskRender interface, containing the Render method, to abstract the visualization logic of tasks.
    
    This enforces the Single Responsibility Principle (SRP) by removing the responsibility of formatting and outputting data from the domain entities (BaseTask, etc.) and delegating it exclusively to the UI layer.

---
+++ TaskMate/TaskMate.UI/ITaskRender.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskMate.Core;
+
+namespace TaskMate.UI
+{
+    internal interface ITaskRender
+    {
+        void Render(BaseTask task);
+    }
+}
